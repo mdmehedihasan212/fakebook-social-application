fc31df7990e145c725bf3ffaa26451af68acf2a3
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-02 23:09:21 +0600

    add profile right bar for user personal info show

--- src/pages/Home/RightBar/RightBar.js
+++ src/pages/Home/RightBar/RightBar.js
@@ -3,33 +3,46 @@ import useUsers from '../../../hooks/useUsers';
 import OnlineFriend from '../OnlineFriend/OnlineFriend';
 import './RightBar.css';
 
-const RightBar = () => {
+const RightBar = ({ profile }) => {
     const [users] = useUsers();
+
+    const HomeRightBar = () => {
+        return (
+            <div className="BirthdayContainer">
+                <h4>Birthdays</h4>
+                <div className="birthdayContent">
+                    <img src="/assets/icon/gift.ico" alt="img" className="birthdayImg" />
+                    <span className="birthdayText">
+                        <strong>Monir Hossain</strong> and <strong>5 others</strong> have birthdays today.
+                    </span>
+                </div>
+                <h4 className="onlineFriendTitle">Online Friends</h4>
+                <ul className="onlineFriendsList">
+                    {
+                        users.map(user => <OnlineFriend
+                            key={user.id}
+                            user={user}
+                        ></OnlineFriend>)
+                    }
+
+                </ul>
+            </div>
+        )
+    };
+
+    const ProfileRightBar = () => {
+        return (
+            <h4>Hey bro how are you?</h4>
+        )
+    }
 
     return (
         <div className='rightBar'>
             <div className="rightBarWrapper">
+
+
                 <h4>Ads</h4>
                 <img src="/assets/ads/1.jpg" alt="img" className="rightBarAdImg" />
-                <div className="BirthdayContainer">
-                    <h4>Birthdays</h4>
-                    <div className="birthdayContent">
-                        <img src="/assets/icon/gift.ico" alt="img" className="birthdayImg" />
-                        <span className="birthdayText">
-                            <strong>Monir Hossain</strong> and <strong>5 others</strong> have birthdays today.
-                        </span>
-                    </div>
-                    <h4 className="onlineFriendTitle">Online Friends</h4>
-                    <ul className="onlineFriendsList">
-                        {
-                            users.map(user => <OnlineFriend
-                                key={user.id}
-                                user={user}
-                            ></OnlineFriend>)
-                        }
-
-                    </ul>
-                </div>
             </div>
         </div>
     );
