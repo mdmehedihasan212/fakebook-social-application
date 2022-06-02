d1ac56bf97272b9fa7cedc0f04eecc812e1be9e4
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-02 16:40:03 +0600

    display user post dynamic

--- src/pages/Home/FeedPost/FeedPost.js
+++ src/pages/Home/FeedPost/FeedPost.js
@@ -4,15 +4,16 @@ import { BsThreeDotsVertical } from 'react-icons/bs';
 
 const FeedPost = ({ post }) => {
     console.log(post);
+    const { name, description, comment, date, image, like, share } = post;
 
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src="/assets/post/1.jpg" alt="" className="postProfileImg" />
-                        <span className="postUserName">Md Monir Hossain</span>
-                        <span className="postTime">5 minute ago</span>
+                        <img src={image} alt="" className="postProfileImg" />
+                        <span className="postUserName">{name}</span>
+                        <span className="postTime">{date}</span>
                     </div>
                     <div className="postTopRight">
                         <BsThreeDotsVertical />
@@ -20,19 +21,19 @@ const FeedPost = ({ post }) => {
                 </div>
                 <div className="postCenter">
                     <span className="userPostText">
-                        Hey! It's my first post
+                        {description}
                     </span>
-                    <img src="/assets/post/1.jpg" alt="" className="userPostImg" />
+                    <img src={image} alt="" className="userPostImg" />
                 </div>
                 <div className="postBottom">
                     <div className="postLeft">
                         <img src="/assets/icon/thums.ico" alt="" className="postLikeIcon" />
                         <img src="/assets/icon/heart.ico" alt="" className="postLikeIcon" />
-                        <span className="postLikeCount">32 people like</span>
+                        <span className="postLikeCount">{like} people like</span>
                     </div>
                     <div className="postRight">
-                        <span className="postComment">9 Comments</span>
-                        <span className="postComment">12 Share</span>
+                        <span className="postComment">{comment} Comments</span>
+                        <span className="postComment">{share} Share</span>
                     </div>
                 </div>
             </div>
