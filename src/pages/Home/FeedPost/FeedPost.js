b7df02ad7d43674d1141c5320f7b2117ee702524
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-02 17:10:36 +0600

    fetch users data and dynamic show user post data

--- src/pages/Home/FeedPost/FeedPost.js
+++ src/pages/Home/FeedPost/FeedPost.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FeedPost.css';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 
 const FeedPost = ({ post }) => {
-    console.log(post);
-    const { name, description, comment, date, image, like, share } = post;
+    const { userId, name, description, comment, date, image, like, share } = post;
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('user.json')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
 
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src={image} alt="" className="postProfileImg" />
-                        <span className="postUserName">{name}</span>
+                        <img src={users.filter(user => user.id === userId)[0]?.image} alt="" className="postProfileImg" />
+                        <span className="postUserName">{users.filter(user => user.id === userId)[0]?.name}</span>
                         <span className="postTime">{date}</span>
                     </div>
                     <div className="postTopRight">
