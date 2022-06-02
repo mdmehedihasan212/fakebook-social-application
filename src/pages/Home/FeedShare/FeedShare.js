0049042a0b45ca4bbc7813b83547f2aa04a25e39
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-02 10:55:56 +0600

    add share img and icon

--- src/pages/Home/FeedShare/FeedShare.js
+++ src/pages/Home/FeedShare/FeedShare.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './FeedShare.css';
+import { MdMovie } from 'react-icons/md';
 
 const FeedShare = () => {
     return (
-        <div>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, perspiciatis?</p>
+        <div className="share">
+            <div className="shareWrapper">
+                <div className="shareTop">
+                    <img src='/assets/post/1.jpg' alt="" className="shareProfileImage" />
+                    <input placeholder="What's is your mind?" className="shareInput" />
+                </div>
+                <hr className='shareHr' />
+                <div className="shareBottom">
+                    <div className="shareOptions">
+                        <div className="shareOption">
+                            <MdMovie className="shareIcon" />
+                            <span className="shareOptionText">
+                                Photo or Video
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
