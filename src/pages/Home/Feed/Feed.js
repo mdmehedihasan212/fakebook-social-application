e462fab005b933fdcafa16eeeb0fbe354fa42ce1
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-02 10:38:59 +0600

    add feed share section

--- src/pages/Home/Feed/Feed.js
+++ src/pages/Home/Feed/Feed.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import FeedShare from '../FeedShare/FeedShare';
 import './Feed.css';
 
 const Feed = () => {
     return (
         <div className='feed'>
-            <h1>Feed</h1>
+            <div className="feedWrapper">
+                <FeedShare></FeedShare>
+            </div>
         </div>
     );
 };
---
+++ src/pages/Home/FeedShare/FeedShare.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FeedShare.css';
+
+const FeedShare = () => {
+    return (
+        <div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, perspiciatis?</p>
+        </div>
+    );
+};
+
+export default FeedShare;
