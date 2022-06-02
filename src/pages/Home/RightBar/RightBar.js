2be75368c155985ae1adf9d1e6420e8816037ab5
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-02 14:57:14 +0600

    add online friends section and img

--- src/pages/Home/RightBar/RightBar.js
+++ src/pages/Home/RightBar/RightBar.js
@@ -7,10 +7,22 @@ const RightBar = () => {
             <div className="rightBarWrapper">
                 <div className="BirthdayContainer">
                     <h4>Birthdays</h4>
-                    <img src="/assets/icon/gift.ico" alt="" className="birthImg" />
-                    <span className="birthText">
-                        <strong>Monir Hossain</strong> and <strong>5 others</strong> have birthdays today.
-                    </span>
+                    <div className="birthdayContent">
+                        <img src="/assets/icon/gift.ico" alt="" className="birthdayImg" />
+                        <span className="birthdayText">
+                            <strong>Monir Hossain</strong> and <strong>5 others</strong> have birthdays today.
+                        </span>
+                    </div>
+                    <img src="/assets/post/1.jpg" alt="" className="rightBarAdImg" />
+                    <h4 className="onlineFriendTitle">Online Friends</h4>
+                    <ul className="onlineFriendsList">
+                        <li className="onlineFriendItem">
+                            <div className="friendsImgContainer">
+                                <img src="/assets/profile/1.jpg" alt="" className="friendsProfileImg" />
+                                <span className="onlineBadge">1</span>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
