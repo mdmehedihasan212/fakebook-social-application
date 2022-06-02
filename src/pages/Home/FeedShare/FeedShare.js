25baae19b4f132e85d2441039f9ed5c2949d2656
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-02 11:28:31 +0600

    add fixed feed img and some react icons

--- src/pages/Home/FeedShare/FeedShare.js
+++ src/pages/Home/FeedShare/FeedShare.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import './FeedShare.css';
-import { MdMovie } from 'react-icons/md';
+import { MdPermMedia } from 'react-icons/md';
+import { AiTwotoneTags } from 'react-icons/ai';
+import { MdLocationOn } from 'react-icons/md';
+import { BsFillEmojiSmileFill } from 'react-icons/bs';
 
 const FeedShare = () => {
     return (
         <div className="share">
             <div className="shareWrapper">
                 <div className="shareTop">
-                    <img src='/assets/post/1.jpg' alt="" className="shareProfileImage" />
+                    <img src='/assets/profile/1.jpg' alt="" className="shareProfileImage" />
                     <input placeholder="What's is your mind?" className="shareInput" />
                 </div>
                 <hr className='shareHr' />
                 <div className="shareBottom">
                     <div className="shareOptions">
                         <div className="shareOption">
-                            <MdMovie className="shareIcon" />
+                            <MdPermMedia className="shareIcon" />
                             <span className="shareOptionText">
                                 Photo or Video
+                            </span>
+                        </div>
+                        <div className="shareOption">
+                            <AiTwotoneTags className="shareIcon" />
+                            <span className="shareOptionText">
+                                Tags
+                            </span>
+                        </div>
+                        <div className="shareOption">
+                            <MdLocationOn className="shareIcon" />
+                            <span className="shareOptionText">
+                                Location
+                            </span>
+                        </div>
+                        <div className="shareOption">
+                            <BsFillEmojiSmileFill className="shareIcon" />
+                            <span className="shareOptionText">
+                                feelings
                             </span>
                         </div>
                     </div>
